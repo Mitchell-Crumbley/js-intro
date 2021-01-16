b9ffb25f731750e84d47c77f2348369ad738a695
Mitchell Crumbley <mitchellcrumbley@gmail.com>
2021-01-16 11:09:05 -0600

    JavaScript working with variables

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-console.log ("JS Intro!");
+console.log ("CONNECTED");
+
+// ********* START VARIABLES *****************
+
+let firstName = 'Beyonce';
+let lastName = 'Knowles';
+
+//console.log(firstName + lastName);
+//console.log (`First Name: ${firstName} Last Name: ${lastName}`);
+
+firstName = 'Mitchell';
+lastName = 'Crumbley';
+
+//console.log(firstName + lastName);
+//console.log (`First Name: ${firstName} Last Name: ${lastName}`);
+
+//const favoriteFood = 'Tacos';
+//console.log(favoriteFood);
+
+//favoriteFood = 'Pizza';
+//console.log (favoriteFood);
+
+// ************ END VARIABLES*****************
+
+// ************ START DATA TYPES *****************
+// const dog = 'bark';
+// console.log(typeof dog);
+
+// const answer = 33;
+// const negative = -14;
+
+// console.log (typeof answer, typeof negative);
+
+// const iLoveJavaScript = true;
+
+// console.log(typeof iLoveJavaScript);
+
+// ************ END DATA TYPES*****************
+
+// ************ START MATH *****************
+
+
+
+// ************ END MATH ***********
